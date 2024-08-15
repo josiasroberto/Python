63b64430de2e8aaf34f30a748af8db237fd982db
Josias Roberto <josiasroberto1@gmail.com>
2024-08-15 18:42:35 -0300

    create logOut and signUp routes

--- sample-flask-auth/app.py
+++ sample-flask-auth/app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, jsonify, request
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_required, login_user, current_user, logout_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -25,7 +25,7 @@ def load_user(user_id):
 
 @app.route('/login', methods=["POST"])
 def login():
-    data = request.get_json()
+    data = request.json
     username = data.get("username")
     password = data.get("password")
 
@@ -39,6 +39,30 @@ def login():
             return jsonify({"message": "Login realizado com sucesso!"})
     
     return jsonify({"message": "Credenciais inválidas"}), 400
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso!"})
+
+@app.route('/user', methods=["POST"])
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user_already_exists = User.query.filter_by(username=username).first()
+        if user_already_exists:
+            return jsonify({"message": "Username já existe"}), 400
+        
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "Usuario cadastrado com sucesso"})
+
+    return jsonify({"message": "Dados invalidos"}), 400
 
 
 @app.route("/hello-world", methods=["GET"])
