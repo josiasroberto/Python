262deae623c8e1978e6ebea8caa44f9719ddaaf3
Josias Roberto <josiasroberto1@gmail.com>
2024-08-15 17:40:48 -0300

    Create user and Authenticate

--- sample-flask-auth/app.py
+++ sample-flask-auth/app.py
@@ -1,13 +1,45 @@
-from flask import Flask
+import os
+from flask import Flask, jsonify, request
 from models.user import User
 from database import db
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'instance/database.db')
+
+# print(f"Database URI: {app.config['SQLALCHEMY_DATABASE_URI']}")
+
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+#view login
+login_manager.login_view = 'login'
 #Session <- conexão ativa
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+@app.route('/login', methods=["POST"])
+def login():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        # Login
+        user = User.query.filter_by(username=username).first()
+                
+        if user and user.password == password:
+            login_user(user)  # Inicia a sessão
+            print(current_user.is_authenticated)
+            return jsonify({"message": "Login realizado com sucesso!"})
+    
+    return jsonify({"message": "Credenciais inválidas"}), 400
+
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
