0ed6fa4020879faaf62e455d03376d655349f69c
Josias Roberto <josiasroberto1@gmail.com>
2024-08-22 14:58:33 -0300

    Daily Diet API Challenge: Add GET one meal, PUT and DELETE routes, finish project

--- daily-diet-api/app.py
+++ daily-diet-api/app.py
@@ -41,5 +41,49 @@ def get_meals():
     }
     return jsonify(output)    
 
+@app.route('/meal/<int:id>', methods=['GET'])
+def get_meal(id):
+    meal = Meal.query.get(id)
+
+    if meal:
+        return jsonify({"meal": meal.to_dict()})
+
+    return jsonify({"message": "Refeição não encontrada!" }), 404
+
+@app.route('/meal/<int:id>', methods=['PUT'])
+def update(id):
+    meal = Meal.query.get(id)
+    data = request.get_json()
+
+    name = data.get("name")
+    description = data.get("description")
+    datetime = data.get("datetime")
+    is_on_the_diet = data.get("is_on_the_diet")
+
+    if not name or not datetime:
+        return jsonify({"message": "Dados inválidos!"}), 400
+
+    if meal:
+        meal.name = name
+        meal.description = description
+        meal.datetime = datetime
+        meal.is_on_the_diet = is_on_the_diet
+        db.session.commit()
+        return jsonify({"message": "Refeição atualizada com sucesso!"}), 200
+    
+    return jsonify({"message": "Refeição não encontrada!"}), 404
+    
+@app.route('/meal/<int:id>', methods=['DELETE'])
+def delete_meal(id):
+    meal = Meal.query.get(id)
+
+    if meal:
+        db.session.delete(meal)
+        db.session.commit()
+        return jsonify({"message": "Refeição excluída com sucesso!"}), 200
+    
+    return jsonify({"message": "Refeição não encontrada!"}), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True)
